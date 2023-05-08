39b884ced08faea5ce6fbef3f22d6f9d0b6fa8b2
bryantobing12 <bryantobing0@gmail.com>
2023-05-08 18:52:30 +0800

    feat: react router dom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { RotateLoader } from "react-spinners";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mountedState, setMountedState] = useState(false);
+
+  useEffect(() => {
+    setMountedState(true);
+  }, []);
+
+  return (
+    <>
+      {mountedState ? (
+        <Component {...pageProps} />
+      ) : (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <RotateLoader color="white" />
+        </div>
+      )}
+    </>
+  );
 }
